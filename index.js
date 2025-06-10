cf992d009cdf473601e09f35b6cceeec0701030d
Chidinma18 <mariamwill01@aol.com>
2025-06-10 04:28:04 -0700

    finished portfolio- may tweak it later. added fun shapes and made responsive

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
 
 
 function toggleContrast() {
@@ -11,7 +24,6 @@ function toggleContrast() {
         document.body.classList.remove("dark-theme")
     }
 }
-
 
 
 function contact(event) {
@@ -39,7 +51,7 @@ function contact(event) {
 
 
 
-function toggleModal() { 
+function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
         return document.body.classList.remove("modal--open");
